f35fdbdce804f8d1b14ba9131c17a0dec6558808
supergirl <supergirl@anniecannons.com>
2021-03-18 13:36:05 -0700

    my second commit for the src folder

---
+++ App.js
@@ -0,0 +1,17 @@
+import Accordion from './Components/Accordion';
+import './App.css';
+
+const App = () => {
+
+  
+  return (
+    <div className="App">
+        <h1>
+        My React Recipe Box is here!
+        </h1>
+        <Accordion />
+    </div>
+  );
+}
+
+export default App;
